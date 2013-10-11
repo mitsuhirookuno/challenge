e03c2118b8b848e56d49e5c6bfe5846de6512959
mitsuhirookuno <mitsuhirookuno@gmail.com>
2013-10-12 00:45:55 +0900

    初期登録

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge::Application.config.secret_key_base = 'b26cd2b513facf532e736548d65ca943753166c043077b103270cad4a56f844a8a854600b64d2fd136180d3e2692756f044f41e66be5500cf837cba1dada0b20'
