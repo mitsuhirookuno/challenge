fa44fec778687499afc5d5f9537572f7961fb9cb
mitsuhirookuno <mitsuhirookuno@gmail.com>
2013-10-21 23:11:40 +0900

    db/schema.rbも追加

--- README.markdown
+++ README.markdown
@@ -4,6 +4,7 @@
 本アプリケーションは、情報が分散しがちな電子書籍の値段を比較したい！と、いう願いを込めて作っています。
 現在のところ、クロール・スクレイピングはしておらず、人力での入力が必要です。
 ```
+
 <a href="http://challenge20131025.herokuapp.com/" target="_blank">こちらでアプリを稼働させています</a>
 
 ## Ruby version
@@ -11,6 +12,29 @@ ruby 2.0.0p247
 
 ## Rails version
 rails 4.0.0
+
+## Local Setting
+
+### GitHub上でFork
+
+```
+https://github.com/mitsuhirookuno/challenge/
+```
+
+### リポジトリをローカルへ作成
+
+```
+cd ~/WORK_DIRECTORY
+git clone git@github.com:YOUR_USER_NAME/APP_NAME.git
+cd APP_NAME
+```
+
+### ブランチ作成
+
+```
+git checkout -b YOUR_NEW_BRANCH
+```
+
 
 ### Rails起動
 
@@ -23,6 +47,7 @@ bundle install --without production
 ローカルDB作成
 
 ```
+rake db:migrate
 rake db:setup
 ```
 
---
+++ db/schema.rb
@@ -0,0 +1,50 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20131021045301) do
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "store_book_prices", force: true do |t|
+    t.integer  "price"
+    t.integer  "store_book_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "store_books", force: true do |t|
+    t.integer "store_id", null: false
+    t.integer "book_id",  null: false
+  end
+
+  create_table "stores", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
