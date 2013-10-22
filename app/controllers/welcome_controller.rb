15a275046514a93c1a96ecd06e7db3556fa0cccd
mitsuhirookuno <mitsuhirookuno@gmail.com>
2013-10-22 20:47:14 +0900

    eagr loadingに

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   skip_before_filter :require_login, :only => [:index, :new, :create]
 
   def index
-    @books = Book.all
+    @books = Book.includes( :stores, :store_books => :store_book_price  ).load
     @stores = Store.all
   end
 
