526feab28b1325b1cddf257c5926f85a9065b5ab
mitsuhirookuno <mitsuhirookuno@gmail.com>
2013-10-23 21:24:13 +0900

    バリデーションを変更中３

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
 
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
-
-  validates :email, uniqueness: true
+  validates :username, presence: { message: '省略できません' }
+  validates :email, presence: { message: '省略できません' },
+                    uniqueness: { message: '既に登録されています' }
+  validates :password, length: { minimum: 3, too_short: "%{count}文字以上の入力が必要です" },
+                       confirmation: { message: '確認用の入力が必要です' }
+  validates :password_confirmation, presence: { message: '省略できません' }
 
 end
